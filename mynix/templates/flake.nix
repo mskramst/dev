0d5b1ec6688625527051af509c304f08ea7e9595
Mike S <mskramst@gmail.com>
2024-03-17 11:26:27 -0700

    Added mynix templates

---
+++ mynix/templates/flake.nix
@@ -0,0 +1,123 @@
+{
+  description = "A collection of flake templates";
+
+  outputs = { self }: {
+
+    templates = {
+
+      trivial = {
+        path = ./trivial;
+        description = "A very basic flake";
+      };
+
+      simpleContainer = {
+        path = ./simple-container;
+        description = "A NixOS container running apache-httpd";
+      };
+
+      python = {
+        path = ./python;
+        description = "Python template, using poetry2nix";
+        welcomeText = ''
+          # Getting started
+          - Run `nix develop`
+          - Run `poetry run python -m sample_package`
+        '';
+      };
+
+      rust = {
+        path = ./rust;
+        description = "Rust template, using Naersk";
+      };
+
+      bash-hello = {
+        path = ./bash-hello;
+        description = "An over-engineered Hello World in bash";
+      };
+
+      c-hello = {
+        path = ./c-hello;
+        description = "An over-engineered Hello World in C";
+      };
+
+      rust-web-server = {
+        path = ./rust-web-server;
+        description = "A Rust web server including a NixOS module";
+      };
+
+      compat = {
+        path = ./compat;
+        description = "A default.nix and shell.nix for backward compatibility with Nix installations that don't support flakes";
+      };
+
+      haskell-hello = {
+        path = ./haskell-hello;
+        description = "A Hello World in Haskell with one dependency";
+      };
+
+      hercules-ci = {
+        path = ./hercules-ci;
+        description = "An example for Hercules-CI, containing only the necessary attributes for adding to your project.";
+      };
+
+      full = {
+        path = ./full;
+        description = "A template that shows all standard flake outputs";
+        welcomeText = ''
+          You just created a template that will show you all standard flake outputs.
+
+          Read more about it here:
+
+            https://github.com/NixOS/templates/tree/master/full
+        '';
+      };
+
+      pandoc-xelatex = {
+        path = ./pandoc-xelatex;
+        description = "A report built with Pandoc, XeLaTex and a custom font";
+      };
+
+      go-hello = {
+        path = ./go-hello;
+        description = "A simple Go package";
+      };
+
+      empty = {
+        path = ./empty;
+        description = "A flake with no outputs";
+      };
+
+      haskell-nix = {
+        path = ./haskell.nix;
+        description = "An haskell.nix template using hix";
+        welcomeText = ''
+          You just created an haskell.nix template using hix. Read more about it here:
+          https://input-output-hk.github.io/haskell.nix/tutorials/getting-started-flakes.html
+        '';
+      };
+
+      haskell-flake = {
+        path = ./haskell-flake;
+        description = "A haskell-flake template";
+        welcomeText = ''
+          You just created a haskell-flake template.
+          See the README or https://github.com/srid/haskell-flake.
+        '';
+      };
+
+      utils-generic = {
+        path = ./utils-generic;
+        description = "Simple, all-rounder template with utils enabled and devShell populated";
+      };
+
+      dotnet = {
+        path = ./dotnet;
+        description = "A .NET application and test project";
+      };
+
+    };
+
+    defaultTemplate = self.templates.trivial;
+
+  };
+}
